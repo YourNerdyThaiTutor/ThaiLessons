03cd708c74cc27f8f10bbd3c7df00020899a0f8f
charineec <charineeemail@gmail.com>
2025-05-09 13:49:14 -0700

    index

---
+++ script.js
@@ -0,0 +1,89 @@
+function onPageLoaded() {
+    // Write your javascript code here
+    console.log("page loaded");
+    
+}
+
+var consonants =["ก","จ","ด","ต","บ","ป","อ"];
+var tones =["","่","้","๊","๋"];
+var vowels =["า","อ","ู"];
+var div = document.getElementById("answers");
+var choices = [];
+var correct ="";
+var score = 0;
+var alreadyWrong = false;
+var streak = 0;
+
+function generateChoiceButton(answer){
+    var x = document.createElement("BUTTON");
+    var t = document.createTextNode(answer);
+    x.appendChild(t);
+    x.id = answer;
+    x.onclick = function(){checkAnswer(answer);};
+    div.appendChild(x);
+}
+
+function generateAnswerChoices(){
+    //var c = Math.floor(Math.random() * 7);
+    while (div.firstChild) {
+    div.firstChild.remove();
+}
+    choices = [];
+    var c = "ก";
+    var t = Math.floor(Math.random() * 5);
+    var v = Math.floor(Math.random() * 3);
+    for(let i=0; i< tones.length; i++){
+        var word = c + tones[i] + vowels[v];
+        choices.push(word);
+        generateChoiceButton(word);
+    }
+    
+}
+function checkAnswer(txt){
+    
+    
+    if(txt === correct){
+        //var t = document.createTextNode("correct");
+        //document.body.appendChild(t);
+        document.getElementById(txt).style.background='#00ff00';
+        if(!alreadyWrong){
+            score ++;
+            streak ++;
+            document.getElementById("score").textContent = "Score: "+score;
+            document.getElementById("streak").textContent = "Streak: "+streak;
+        }
+        else{
+            streak = 0;
+            document.getElementById("streak").textContent = "Streak: "+streak;
+        }
+        alreadyWrong = false;
+        startGame();
+    }
+    else{
+        //var ti = document.createTextNode("wrong");
+        //document.body.appendChild(ti);
+        document.getElementById(txt).style.background='#ff3333';
+        alreadyWrong = true;
+    }
+}
+function createQuestion(){
+    var correctIndex = Math.floor(Math.random() * choices.length);
+    correct = choices[correctIndex];
+    var src = "audios/"+correct+".mp3";
+    
+    var audio = document.getElementById('audio');
+
+    var source = document.getElementById('audioSource');
+    source.src = src;
+    audio.load();
+    audio.play();
+    //var t = document.createTextNode(src);
+    //document.body.appendChild(t);
+}
+
+function startGame(){
+    generateAnswerChoices();
+    createQuestion();
+}
+
+startGame();
