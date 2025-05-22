d5fd68489265def7e1687ef8a9c1a21b33d90d82
charineec <charineeemail@gmail.com>
2025-05-22 16:31:23 -0700

    updated low tones

--- Lesson1.js
+++ Lesson1.js
@@ -1,14 +1,7 @@
-function onPageLoaded() {
-    // Write your javascript code here
-    console.log("page loaded");
-    
-}
-
-var mid =["ก","จ","ด","ต","บ","ป","อ"]; //middle consonants
+var consonants =["ก","จ","ด","ต","บ","ป","อ"]; //middle consonants
 var single_low_consonants = ["ง","ญ","น","ย","ณ","ร","ว","ม","ฬ","ล"];
 var paired_low_consonants = ["ค","ช","ซ","ท","พ","ฟ","ฮ"];//simple consonants no repeeated sounds
 var paired_high_consonants = ["ข","ฉ","ส","ถ","ผ","ฝ","ห"];
-var consonants = mid;
 var tones =["","่","้","๊","๋"];
 var vowels =["า","อ"];
 var div = document.getElementById("answers");
@@ -20,19 +13,17 @@ var streak = 0;
 var options = ["mid", "single low", "paired", "all"];
 var selected_option = "mid";
 var selected_consonants = consonants;
-var curr_level = "mid";
 
-function generateChoiceButton(answer, _id){
+function generateChoiceButton(answer){
     var x = document.createElement("BUTTON");
     var t = document.createTextNode(answer);
     x.appendChild(t);
-    x.id = _id;
-    x.onclick = function(){checkAnswer(_id);};
+    x.id = answer;
+    x.onclick = function(){checkAnswer(answer);};
     div.appendChild(x);
 }
 
 function generateAnswerChoices(){
-    
     var c = Math.floor(Math.random() * consonants.length);
     while (div.firstChild) {
         div.firstChild.remove();//clear answer choices from previous round
@@ -41,99 +32,17 @@ function generateAnswerChoices(){
     //var c = "ก";
     //var t = Math.floor(Math.random() * consonants.length);
     var v = Math.floor(Math.random() * vowels.length);
-    /*for(let i=0; i< tones.length; i++){
-        var word = consonants[c] + tones[i] + vowels[v];
+    for(let i=0; i< consonants.length; i++){
+        var word = consonants[i];
         choices.push(word);
-        generateChoiceButton(word, word);
-    }*/
-    generateTones(consonants[c],vowels[v]);
+        generateChoiceButton(word);
+    }
     
 }
 
-function generateTones(consonant,vowel){
-    
-    var word = "";
-     var _id = "";
-    if(curr_level==="mid"){
-        for(let i=0; i< tones.length; i++){
-            word = buildWord(consonant, tones[i], vowel);
-            
-        }
-    }
-   
-    else if(curr_level==="single"){
-        
-        word = buildWord(consonant,"",vowel);
-        
-        
-        word = buildWord("ห"+consonant,"่",vowel);
-        
-        
-        word = buildWord(consonant,"่",vowel);
-        document.getElementById(word).innerHTML+="/"+"ห"+consonant+"้"+vowel;
-       
-        
-        word = buildWord(consonant,"้",vowel);
-        
-        
-        word = buildWord("ห"+consonant,"",vowel);
-          
-        
-        
-    }
+function generateTones(){
     
     
-}
-
-function buildWord(c, t,v){
-    var consonant_map={
-        "ง": "ง",
-        "ญ": "ย",
-        "น": "น",
-        "ย": "ย",
-        "ณ": "น",
-        "ร": "ร",
-        "ว": "ว",
-        "ม": "ม",
-        "ฬ":"ล",
-        "ล":"ล",
-        "ก":"ก",
-        "จ":"จ",
-        "ด":"ด",
-        "ต":"ต",
-        "บ":"บ",
-        "ป":"ป",
-        "อ":"อ"
-    };
-    var word = c+t+v;
-    var _id = "";
-    if(c.length>1){
-        if(c[0]==="ห"){
-            _id = "ห"+consonant_map[c.slice(-1)]+t+v;
-        }
-    }
-    else if(c.length===1){
-        _id = consonant_map[c]+t+v;
-    }
-    choices.push(_id);
-    generateChoiceButton(word,_id);
-    return _id;
-}
-
-function selectLevel(level){
-    document.getElementById(curr_level).className ="options";
-    document.getElementById(level).className = "selectedOption";
-    curr_level = level;
-    if(level==="mid"){
-        consonants = mid;
-    }
-    else if(level === "single"){
-        consonants = single_low_consonants;
-    }
-    else if(level === "paired"){
-        consonants = paired_low_consonants;
-    }
-    startGame();
 }
 
 function checkAnswer(txt){
@@ -168,8 +77,8 @@ function checkAnswer(txt){
 function createQuestion(){
     var correctIndex = Math.floor(Math.random() * choices.length);
     correct = choices[correctIndex];
+    var src = "audios/"+correct+"อ"+".mp3";
     
-    var src = curr_level+"/"+correct+".mp3";
     var audio = document.getElementById('audio');
 
     var source = document.getElementById('audioSource');
@@ -192,7 +101,7 @@ function playAllTones(){
     document.getElementById("helper").textContent = "play next";
     var button = choices[current_tone];
     document.getElementById(button).style.background='#00a5f6';
-    var src = curr_level+"/"+button+".mp3";
+    var src = "audios/"+button+"อ"+".mp3";
     
     var audio = document.getElementById('helper_audio');
 
@@ -203,7 +112,7 @@ function playAllTones(){
     current_tone ++;
     if(current_tone===choices.length){
         current_tone = 0;
-        document.getElementById("helper").textContent = "click here to hear all tones";
+        document.getElementById("helper").textContent = "click here to hear all letters";
         for(let i = 0;i<choices.length;i++){
             document.getElementById(choices[i]).style.background='#ffffff';
         }
@@ -214,6 +123,3 @@ function playAllTones(){
 
 startGame();
 document.getElementById("helper").onclick = function(){playAllTones();};
-document.getElementById("mid").onclick = function(){selectLevel("mid");};
-document.getElementById("single").onclick = function(){selectLevel("single");};
-document.getElementById("paired").onclick = function(){selectLevel("paired");};
