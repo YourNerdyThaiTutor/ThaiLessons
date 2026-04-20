6fed685e21bdb667a05489baef1be1918f518d70
nerdyGuava <quantumreece@gmail.com>
2026-04-19 21:00:33 -0700

    cross vocab

---
+++ vocabGames/assets/vocabWords.js
@@ -0,0 +1,245 @@
+const vocabWords = `ศูนย์,0
+หนึ่ง,1
+สอง,2
+สาม,3
+สี่,4
+ห้า,5
+หก,6
+เจ็ด,7
+แปด,8
+เก้า,9
+สิบ,10
+ร้อย,hundred
+พัน,thousand
+หมื่น,ten thousand
+แสน,hundred thousand
+ล้าน,million
+ยี่สิบ,20
+เอ็ด,used when it follows a ten. You'll still use หนึ่ง if there's no ten
+อยู่ที่ไหน,where is+(place)
+อยู่ไหน,where is+(place) [more casual]
+อยู่,is at a location
+ห้องน้ำ,restroom
+ที่แลกเงิน,money exchange place
+รถไฟฟ้า,Sky Train
+รถใต้ดิน,"Subway" or "underground train"
+ขอบคุณ,Thank you
+สุขา,restroom (formal)
+เลี้ยว,to turn
+ทาง,"Way" or "direction of" or "on the(direction)"
+ตรงไป,Go straight
+ซ้าย,Left
+ขวา,Right
+แล้ว,Then
+ข้าง,"Side" or "right next to"
+ข้างหน้า,"Straight ahead" or "right in front"
+ชั้น,Level (floor)
+เบียร์,beer
+สัตว์,animal
+มนุษย์,human
+ศูนย์,zero (0)
+สัปดาห์,week (formal)
+ตังค์,money (casual)
+เบอร์,number
+พิมพ์,to type
+คอมพิวเตอร์,computer
+โชว์,to show
+ลิฟท์,"elevator" or "lift"
+โทรศัพท์,phone
+พระจันทร์,the moon
+พระอาทิตย์,the sun
+วันศุกร์,Friday
+แอร์,Air conditioning
+รถเมล์,a bus
+ทัวร์,tour
+ม่วง,purple
+ฟ้า,light blue
+น้ำเงิน,dark blue
+เขียว,green
+เหลือง,yellow
+ส้ม,orange
+แดง,red
+ชมพู,pink
+เงิน,silver
+ทอง,golden
+เทา,gray
+น้ำตาล,brown
+ขาว,white
+ใส,clear
+ทองแดง,"copper" or "bronze"
+ดำ,black
+เสื้อ,Shirt
+กางเกง,Pants
+เสื้อชั้นใน,Bra
+กางเกงใน,Underwear
+รองเท้า,Shoes
+หมวก,Hat
+แว่นกันแดด,Sunglasses
+แว่น,Glasses
+ถุงเท้า,Socks
+ผ้ากันเปื้อน,Apron
+นาฬิกาข้อมือ,Watch
+ผ้าพันคอ,Scarf
+เสื้อกันหนาว,Sweater
+กระเป๋า,"Purse" or "pocket" or "bag"
+รองเท้าผ้าใบ,Sneakers
+รองเท้าส้นสูง,High heels
+รองเท้าแตะ,"Slippers" or "flip flops"
+ชุดนอน,Pajamas
+กระโปรง,Skirt
+นี้,this+(object)
+นั้น,that+(object)
+โน้น,that(over there)+(object)
+นี่,this/ here
+นั่น,that/ there
+โน่น,that over there/ over there
+อันนี้คืออะไร,What is this?
+เสื้อตัวนั้นราคาเท่าไหร่,How much is the price for that shirt?
+กระเป๋าใบโน้นมีสีอะไรบ้าง,What colors does that bag over there come in?
+หมวกเธออยู่นี่,Your hat is over here.
+นั่น คืออะไร,What is that?
+เห็นโน่นมั้ย,Do you see that over there?
+ตัว,"Literal translation means ""body"". so used for something that has a body such as animals or clothings that is a shape of your body"
+ผืน,"Means a ""sheet"". used for flat things that come in sheets; such as paper or sheets or fabric."
+คู่,"Means ""pair"". for things that come in pairs; such as shoes or earrings or couples."
+ข้าง,"Means ""side"". for things that are supposed to come in pairs; but when talking about just one of them."
+ชุด,"Means ""set"". for things that come in sets; such as outfits or combo meals."
+เรือน,"Literal translation is a house or a box with many components inside just like a building. Back in the days clocks were just big boxes with many different components inside. so they use this classifiers for clocks and watches as well."
+ใบ,"Literal translation is a leaf. so it's used for flat things or things that can contain other things such as hats(contains my head) or a bag or a cup or a bowl."
+อัน,"piece. one(unit of measurement)"
+นี้,this+(classifier)
+เงิน,money
+ตังค์,Money (casual)
+เงินสด,Cash
+บาท,Thai Baht (currency)
+สวัสดี,Hello
+มี,Have
+กี่,how much
+เท่าไหร่,how much
+บัตรเครดิต,Credit card
+ใช้,use
+ได้,can
+ใช่,Yes (correct)
+ไม่,"Negation (no or don't...)"
+ไหม,? (Put this at the end of sentence to make it a question)
+มั้ย,? (same thing as ไหม but more casual)
+เท่านั้น,only
+อันนี้กี่บาทคะ,How much is this?
+ใช้บัตรเครดิตได้มั้ยคะ,Can I use credit card?
+ขยะ,trash
+จมูก,nose
+สมุด,notebook
+องุ่น,grape
+ตลาด,market
+พยาบาล,nurse
+ถนน,road
+สบาย,comfortable
+สวัสดี,hello
+สถานี,station
+ตลก,funny
+ขนม,snack
+ขโมย,steal
+เทวดา,angel
+โทรศัพท์,telephone
+โฆษณา,advertisement
+ทรยศ,betray
+มรสุม,storm
+สกปรก,dirty
+จักรยาน,bicycle
+สุขภาพ,health
+ผลิตภัณฑ์,product
+จักจั่น,cicada
+สกี,ski
+สแปม,spam
+สแกน,scan
+สตรอว์เบอร์รี,strawberry
+แสตมป์,stamp
+เสนอ,present/suggest
+เกษตร,argriculture
+เผยอ,ajar
+อาหาร,Food/cuisine
+ข้าว,Rice/food
+น้ำ,Drinks/liquid
+น้ำเปล่า,Water
+ก๋วยเตี๋ยว,Noodles
+ขนมปัง,Bread
+เกลือ,Salt
+พริก,Chilli
+พริกไทย,Pepper
+ข้าวสวย,Cooked rice
+ข้าวเหนียว,Sticky rice
+นม,Milk
+ไข่,Egg
+ไก่,Chicken
+หมู,Pig/pork
+ทะเล,Sea/seafood
+เนื้อ,Beef/meat
+วัว,Cow
+เป็ด,Duck
+กุ้ง,Shrimp/prawn
+ปลา,Fish
+ปู,Crab
+หอย,Clam
+แพ้,Allergic to
+ถั่ว,Nuts
+ถั่วลิสง,Peanuts
+งา,Sesame
+กลูเตน,Gluten
+อาหารทะเล,Seafood
+กุ้ง,Shrimp
+สัตว์มีกระดอง,Shellfish
+กระดอง,Shell
+ข้าวสาลี,Wheat
+ไข่,Egg
+ไม่ใส่,Don't put in
+กินไม่ได้,Can't eat
+ทุกชนิด,Every type
+ไม่ใส่ถั่วนะคะ กินไม่ได้ แพ้,Please don't put peanuts in. I can't eat. I'm allergic.
+แพ้ถั่วทุกชนิด,I'm allergic to every type of nuts
+ไม่ใส่กุ้งนะคะ แพ้สัตว์มีกระดอง,Please don't put shrimps in. I'm allergic to shellfish.
+หวาน,Sweet
+เปรี้ยว,Sour
+เค็ม,Salty
+ขม,Bitter
+มัน,Oily
+เผ็ด,Spicy
+นัว,Umami
+อร่อย,Delicious
+ไม่อร่อย,Not good
+รสชาติ,Taste
+แปลก,Strange
+น้อย,Less
+นิดหน่อย,Little
+ค่อนข้าง,Quite
+มาก,Very
+ไม่ค่อย,Not quite
+ไป,Too
+ไปหน่อย,A little too
+ไม่พอ,Not enough
+เอา,To take
+ดี,Good
+เอาหวานน้อยค่ะ,I'll take less sweet
+เอาไม่ค่อยเผ็ดนะคะ,Can I have not so spicy?
+ขมไป ไม่อร่อย,"Too bitter. doesn't taste good."
+ค่อนข้างเปรี้ยว,Quite sour
+มันไปหน่อย,A little too oily
+เค็มไม่พอ,Not salty enough.
+ไม่ค่อยนัว รสชาติแปลก,"Not so umami. tastes weird."
+อร่อยมาก เค็มดี,"Very delicious. good kind of salty"
+เอาเผ็ดนิดหน่อย,I'll take just a little spicy
+เผ็ดมากไปไม่อร่อย,Too spicy doesn't taste good.
+ผัดไทย,Pad Thai
+กะเพรา,Kaprao
+ต้มยำ,Tom Yum
+แกงเขียวหวาน,Green Curry
+พะแนง,Panang
+ผัดขี้เมา,drunken stirfry
+หมูปิ้ง,Moo Ping (grilled pork)
+ไก่ย่าง,Grilled Chicken
+ส้มตำ,Papaya Salad
+ลาบ,Larb
+ไส้กรอกอีสาน,Isan Sausage
+ข้าวซอย,Kao Soi
+ไส้อั่ว,Sai Uo
+ก๋วยเตี๋ยวเรือ,Boat Noodles
+ผัดซีอิ๊ว,Pad See Ew`
